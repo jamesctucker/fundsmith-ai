c55bc1a30573219d2d984ca9c00f74178726cfd8
James Tucker <jamesctucker@protonmail.com>
2022-12-23 11:18:49 -0600

    fix import issues

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-const Header = dynamic(() => import("./header"), { ssr: false });
+const Header = dynamic(() => import("./Header"), { ssr: false });
 
 type Props = {
   children: React.ReactNode;
