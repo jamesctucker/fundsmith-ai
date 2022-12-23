1fbb682a93a608666a151dae127665915d51a573
James Tucker <jamesctucker@protonmail.com>
2022-12-23 13:11:20 -0600

    add toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,27 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Layout from "../components/Layout";
-
 import {
   createBrowserSupabaseClient,
   type Session,
 } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/globals.css";
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  const { toasts } = useToasterStore();
+  const TOAST_LIMIT = 3;
+
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible)
+      .filter((_, i) => i >= TOAST_LIMIT)
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
 
   return (
     <>
@@ -24,6 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <Layout>
           <Component {...pageProps} />
+          <Toaster />
         </Layout>
       </SessionContextProvider>
     </>
