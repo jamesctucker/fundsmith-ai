277bf70a88360b07401f826cbe6912a3db8f8d99
James Tucker <jamesctucker@protonmail.com>
2022-12-23 13:02:28 -0600

    setup verify email page

--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -29,7 +29,7 @@ export default function SignUp() {
     if (session) {
       router.push("/");
     } else {
-      router.push("/verify");
+      router.push("/auth/verify");
     }
   };
 
---
+++ src/pages/auth/verify/index.tsx
@@ -0,0 +1,12 @@
+import Wrapper from "@/components/Wrapper";
+
+export default function Verify() {
+  return (
+    <Wrapper title={"Verify"}>
+      <h1>
+        Check your email and click the link in the message to activate your
+        account.
+      </h1>
+    </Wrapper>
+  );
+}
