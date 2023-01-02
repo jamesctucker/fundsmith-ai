4e4023503bbe8c0b9ab07594ba4db185a9e8e8ff
James C. Tucker <hellojamestucker@gmail.com>
2023-01-01 18:37:48 -0600

    first stab at generating variants with gpt-3 (a thank you email)

--- src/pages/new/[contentType].tsx
+++ src/pages/new/[contentType].tsx
@@ -4,11 +4,13 @@ import { toast } from "react-hot-toast";
 import ParameterForm from "@/components/forms/ParameterForm";
 import DefaultFields from "@/components/content-type/DefaultFields";
 import { useForm, FormProvider } from "react-hook-form";
+import { useState } from "react";
 
 const NewPage = () => {
   const methods = useForm();
   const router = useRouter();
   const contentTypeName = router.query.contentType as string;
+  const [variants, setVariants] = useState<string[]>([]);
 
   const { data, error } = trpc.content_types.getContentTypeParameters.useQuery(
     {
@@ -21,8 +23,30 @@ const NewPage = () => {
     toast.error("Oops, something went wrong");
   }
 
-  const onSubmit = async (data: any) => {
+  // trpc mutation that calls generateVariants in the prompt.tsx router
+  const generateVariants = trpc.prompts.generateVariants.useMutation();
+
+  const onSubmit = (data: any) => {
     console.log(data);
+
+    generateVariants.mutate(
+      {
+        content_type: contentTypeName,
+        organization_name: data.organization_name,
+        support_description: data.support_description,
+        supported_project: data.supported_project,
+        tone: data.tone,
+        // prompt: "Who is Abraham Lincoln?",
+        max_tokens: 1000,
+        n: Number(data.numberOfVariants),
+      },
+      {
+        onSuccess: (data) => {
+          console.log("data", data);
+          setVariants(data.variants);
+        },
+      }
+    );
   };
 
   return (
@@ -32,7 +56,7 @@ const NewPage = () => {
         <div className="px-4 py-5 sm:p-6">
           <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(onSubmit)}>
-              {data && <DefaultFields data={data} />}
+              {data && <DefaultFields />}
               {data && <ParameterForm parameters={data.parameters} />}
               <button className=" btn-primary my-2" type="submit">
                 Generate
@@ -42,10 +66,17 @@ const NewPage = () => {
         </div>
       </div>
       {/* list of variation results */}
-      <div className="overflow-hidden rounded-md bg-secondary mt-4 max-w-3xl mx-auto">
+      <div className="overflow-visible rounded-md bg-secondary mt-4 max-w-3xl mx-auto">
         <div className="px-4 py-5 sm:p-6">
           {/* TODO: display a series of random copy saying things like "cooking something up in the kitchen..." */}
-          <p>loading...</p>
+          {generateVariants.isLoading && <p>Loading...</p>}
+          {variants.length > 0 && (
+            <ul>
+              {variants.map((variant, index) => (
+                <li key={index}>{variant}</li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </>
--- src/server/routers/prompt.tsx
+++ src/server/routers/prompt.tsx
@@ -1,27 +1,73 @@
 import { z } from "zod";
-
 import { router, protectedProcedure } from "../trpc";
 
+type Choice = {
+  text: string;
+  index: 0;
+  finish_reason: string;
+};
+
 export const promptsRouter = router({
-  //   send params to OpenAI as part of a prompt and get back variants
+  // TODO: figure out how to create reusable prompt templates that we can inject our inputs into
   generateVariants: protectedProcedure
     .input(
       z.object({
-        name: z.string(),
-        numberOfVariants: z.number(),
+        content_type: z.string(),
+        organization_name: z.string(),
+        support_description: z.string(),
+        supported_project: z.string(),
         tone: z.string(),
+        // prompt: z.string(),
+        max_tokens: z.number(),
+        n: z.number(),
       })
     )
-    .query(async ({ ctx, input }) => {
-      const contentType = await ctx.prisma.contentType.findFirst({
-        where: {
-          name: input.name,
-        },
-        include: {
-          parameters: true,
-        },
-      });
+    .output(z.object({ variants: z.array(z.string()) }))
+    .mutation(async ({ input, ctx }) => {
+      const {
+        content_type,
+        // prompt,
+        max_tokens,
+        n,
+        organization_name,
+        support_description,
+        supported_project,
+        tone,
+      } = input;
 
-      return contentType;
+      const finalPrompt = `You're assisting a fundraiser who works for a non-profit organization called ${organization_name}. 
+                           You are tasked with writing a ${content_type} that will be sent to donors who have supported the organization.
+                           Please use the following information to help you write the ${content_type}:
+
+                           The project the donor supported: ${supported_project}
+                           The type of support the donor provided (mention this in the email): ${support_description}
+
+                           The tone of the ${content_type} should be ${tone}.
+
+                           Please keep this under 300 words.`;
+
+      const res = await fetch("https://api.openai.com/v1/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+        },
+        body: JSON.stringify({
+          model: "text-davinci-003",
+          prompt: finalPrompt,
+          max_tokens: max_tokens,
+          temperature: 0.5,
+          n: n,
+          // we send this to Open AI so they can track users who abuse the API
+          user: ctx.user.id,
+        }),
+      });
+      const body = await res.json();
+      console.log("body", body);
+
+      // go through each choice and return the text
+      const variants = body.choices.map((choice: Choice) => choice.text);
+
+      return { variants: variants };
     }),
 });
