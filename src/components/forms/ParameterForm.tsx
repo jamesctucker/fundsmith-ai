4ad45386c0a369d3437a5d875ea58d6b4cdf0431
James C. Tucker <hellojamestucker@gmail.com>
2023-01-03 08:49:18 -0600

    first stab at an input quality status bar

--- src/components/forms/ParameterForm.tsx
+++ src/components/forms/ParameterForm.tsx
@@ -1,11 +1,16 @@
 import { Parameter, Prisma } from "@prisma/client";
 import { useFormContext } from "react-hook-form";
+import { useState } from "react";
 
 type ParameterProps = {
   parameters: Parameter[];
 };
 
 const ParameterForm = ({ parameters }: ParameterProps) => {
+  const [characterCount, setCharacterCount] = useState<Record<string, number>>(
+    {}
+  );
+
   const {
     register,
     formState: { errors },
@@ -32,6 +37,47 @@ const ParameterForm = ({ parameters }: ParameterProps) => {
     );
 
     countLabel.innerText = `${currentCount}/${maxLength}`;
+
+    setCharacterCount((prevState) => ({
+      ...prevState,
+      [e.target.id]: currentCount,
+    }));
+  };
+
+  const getQuality = (id: string) => {
+    const maxLength = getRules(
+      parameters.find((parameter) => parameter.name === id)!,
+      "maxLength"
+    );
+
+    if (!characterCount[id]) {
+      return;
+    }
+
+    const percentage = (characterCount[id]! / maxLength) * 100;
+
+    return (
+      <div>
+        {percentage < 10 && (
+          <div
+            className="h-2 rounded-full bg-error"
+            style={{ width: "33.3%" }}
+          />
+        )}
+        {percentage >= 10 && percentage < 25 && (
+          <div
+            className="h-2 rounded-full bg-warning"
+            style={{ width: "66.6%" }}
+          />
+        )}
+        {percentage >= 25 && (
+          <div
+            className="h-2 rounded-full bg-success"
+            style={{ width: "100%" }}
+          />
+        )}
+      </div>
+    );
   };
 
   return (
@@ -72,6 +118,16 @@ const ParameterForm = ({ parameters }: ParameterProps) => {
                   This field is required.
                 </p>
               )}
+              <div className="input-quality-status-bar mt-2" aria-hidden="true">
+                <div className="overflow-hidden rounded-full bg-gray-200">
+                  {getQuality(parameter.name)}
+                </div>
+                {/* <div className="mt-6 hidden grid-cols-4 text-sm font-medium text-gray-600 sm:grid">
+                  <div className="text-left text-error">Weak</div>
+                  <div className="text-center text-warning">Good</div>
+                  <div className="text-right text-success">Great!</div>
+                </div> */}
+              </div>
             </>
           )}
           {parameter.displayType === "TEXT" && (
