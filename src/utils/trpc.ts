4648aa6862204b43a4d530349b9d291ab908f7b7
James C. Tucker <hellojamestucker@gmail.com>
2023-01-10 13:58:20 -0600

    add Railway staging url

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -8,6 +8,8 @@ import { type AppRouter } from "../server/routers/_app";
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.RAILWAY_URL) return `https://${process.env.RAILWAY_URL}`; // SSR should use vercel url
+  if (process.env.RAILWAY_STAGING_URL)
+    return `https://${process.env.RAILWAY_STAGING_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
