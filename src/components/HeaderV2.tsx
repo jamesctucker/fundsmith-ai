e48d0611da9901ebd98ffd7db13b03abc3683a90
James Tucker <jamesctucker@protonmail.com>
2023-01-10 12:10:06 -0600

    fix mobile nav link bug

--- src/components/HeaderV2.tsx
+++ src/components/HeaderV2.tsx
@@ -217,21 +217,21 @@ export default function HeaderV2() {
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="/"
                 className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
               >
                 Dashboard
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="/documents"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
               >
                 Documents
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
+                href="/projects"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700"
               >
                 Projects
