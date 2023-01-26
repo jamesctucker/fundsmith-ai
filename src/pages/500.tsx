4b7305a7d04f3d0c9def0c0c4f5d59f2fcdcfb59
James Tucker <jamesctucker@protonmail.com>
2023-01-25 18:56:26 -0600

    add custom 500 page

---
+++ src/pages/500.tsx
@@ -0,0 +1,9 @@
+const ServerErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center h-96">
+      <h1>500 - Ope, something went wrong.</h1>
+    </div>
+  );
+};
+
+export default ServerErrorPage;
