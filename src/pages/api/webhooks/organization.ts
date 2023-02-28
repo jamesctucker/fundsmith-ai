1c29c4ea39f5fac7e95f1c2f95412e64a47f0ea0
James C. Tucker <hellojamestucker@gmail.com>
2023-02-28 09:07:15 -0600

    initial logic for updating OrganizationsUsers table

---
+++ src/pages/api/webhooks/organization.ts
@@ -0,0 +1,61 @@
+import { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { buffer } from "micro";
+
+// Disable the bodyParser so we can access the raw
+// request body for verification.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret: string = process.env.USER_WEBHOOK_SECRET!;
+
+export default async function handler(
+  req: NextApiRequestWithSvixRequiredHeaders,
+  res: NextApiResponse
+) {
+  // Verify the webhook signature
+  // See https://docs.svix.com/receiving/verifying-payloads/how
+  const payload = (await buffer(req)).toString();
+  const headers = req.headers;
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+  try {
+    evt = wh.verify(payload, headers) as Event;
+  } catch (_) {
+    return res.status(400).json({});
+  }
+
+  // Handle the webhook
+  if (evt.type === "organization.created") {
+    if (evt.data.id) {
+      //   create db organization
+      //   create OrganizationsUsers record with organizationId, userId, and role
+    }
+  }
+
+  res.json({});
+}
+
+type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
+  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
+};
+
+// Generic (and naive) way for the Clerk event
+// payload type.
+type Event = {
+  data: EventOrganizationData;
+  object: "event";
+  type: EventType;
+};
+
+type EventOrganizationData = {
+  id: string;
+  name: string;
+  created_by: string;
+};
+
+type EventType = "organization.created" | "organization.updated" | "*";
